497c8e97665ded9872ba6f90df00daed15f6687f
HateR <iwilly17@gmail.com>
2021-03-31 16:34:38 +0300

    Add url generator

--- get_url.py
+++ get_url.py
@@ -1,14 +1,25 @@
-import requests
-from bs4 import BeautifulSoup, SoupStrainer
+import re
 
-url = 'https://cdiak.archives.gov.ua/baza_geog_pok/church.php'
-r = requests.get(url)
-urls = []
-soup = BeautifulSoup(r.content, 'html.parser', parse_only=SoupStrainer('a'))
-for link in soup:
-    if link.has_attr('href'):
-        urls.append(link['href'])
-start = 28
-for start in range(len(urls)):
-    if start >= 28:
-        print(urls[start])
+import requests
+import bs4
+
+
+def get_url():
+    urls = []
+    data = requests.request('get', 'https://cdiak.archives.gov.ua/baza_geog_pok/church.php/')  # any website
+    s = bs4.BeautifulSoup(data.text, 'html.parser')
+    for link in s.findAll('a'):
+        if link.has_attr('href'):
+            link = (str(link)[str(link).find("=\"") + 2:str(link).find("\">")])
+            urls.append("https://cdiak.archives.gov.ua/baza_geog_pok/" + link)
+    return urls
+
+
+def print_urls(url):
+    for i in range(len(url)):
+        print(url[i])
+    return None
+
+
+if __name__ == '__main__':
+    print_urls(get_url())
